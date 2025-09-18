38e1af09c1997326d9db81396a0ce79691373317
Chandre-Leigh-Davids-2 <chandreleighdavids.17@gmail.com>
2025-09-18 10:49:05 +0200

    Home Page edits

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,14 +5,16 @@
         <div class="col-md-7 p-5">
           <h1 class="coiny mb-5">WELCOME TO MY PORTFOLIO!</h1>
           <h1 class="mont-primary mb-5">Hi there!<br>I am <span class="accent">Chandré Leigh Davids</span>.</h1>
-          <p class="mb-5 noto-primary" style="width: 70%; font-weight: 300;">I'm a full-stack developer who builds more than just websites, I craft digital&nbsp;experiences. Whether it's a sleek landing page or a complex web&nbsp;app, I bring together design and code to create products that make&nbsp;an&nbsp;impact.</p>
+          <p class="mb-5 noto-primary home-paragraph" style="font-weight: 300;">I'm a full-stack developer who builds more than just websites, I craft digital&nbsp;experiences. Whether it's a sleek landing page or a complex web&nbsp;app, I bring together design and code to create products that make&nbsp;an&nbsp;impact.</p>
           <div class="d-flex">
             <button class="button learn-more" style="margin-right: 10px;">LEARN MORE</button>
             <button class="button view-cv">VIEW MY CV</button>
           </div>
         </div>
-        <div class="col-md-5 p-0 pt-5" style="padding-right: 3rem!important;">
-          <p>WELCOME TO MY PORTFOLIO!</p>
+        <div class="col-md-5 d-flex justify-content-end align-content-start" style="padding-right: 3rem!important;">
+          <div class="home-page-container d-flex justify-content-end align-content-start">
+            <img src="../assets/images/image-of-self.png" alt="" class="home-image"></img>
+          </div>
         </div>
       </div>
   </div>
