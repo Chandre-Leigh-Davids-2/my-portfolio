a0014cf6a25bbb6cb4e9b96f254c2e0c24f208cd
Chandre-Leigh-Davids-2 <chandreleighdavids.17@gmail.com>
2025-09-27 20:32:52 +0200

    edit

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,11 +1,28 @@
 <template>
-  <div class="container-fluid home-page">
-  <p>Contact</p>
-  <!-- Don't forget to take a picture for the Figma Templagte on this page and use form spree for the contact section-->
+  <div class="container-fluid about-page-primary">
+    <div class="row">
+      <div class="col-md-4 contact-img-div">
+        <!-- <img src="../assets/images/image-of-self.png" alt=""> -->
+         text
+      </div>
+      <div class="col-md-8 p-5">
+        <h1 class="mont-primary">Let’s Make Something Great!</h1>
+        <div class="row">
+          <div class="col-md-6">
+            <h3>Reach Out Anytime</h3>
+            <div class="line"></div>
+          </div>
+          <div class="col-md-6"></div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+
+export default {
+};
 </script>
 
 <style src="../assets/styles/styles.css"></style>
