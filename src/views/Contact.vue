9991f72333329cde8f291be6a92286b9a35f98b5
Chandre-Leigh-Davids-2 <chandreleighdavids.17@gmail.com>
2025-09-29 20:30:34 +0200

    comment

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -2,6 +2,7 @@
   <div class="container-fluid about-page-primary">
     <div class="row">
       <div class="col-md-4 contact-img-div">
+        <!-- Need to get a picture for this, currently working on other projects too -->
         <!-- <img src="../assets/images/image-of-self.png" alt=""> -->
          text
       </div>
